9e1a0ed298824ed54c1ab3ee8457a2c881645a7b
RoomierCG <alex.granda2@hotmail.com>
2024-03-25 14:55:39 +0100

    Part-1/introduction: Dynamic attributes

--- src/routes/basic-svelte/+page.svelte
+++ src/routes/basic-svelte/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     let name = "world";
+    let src = "/favicon.png";
 </script>
 
 <h1>Basic svelte</h1>
@@ -10,3 +11,9 @@
 </p>
 
 <p>Ejemplo de uso de script: "hello {name}"</p>
+
+
+<p>Podemos editar atributos de elemtos html, por ejemplo la siguiente etiqueta: </p>
+<p>Si el atributo y el nombre de la variable coninciden podemos acortar en este ejemplo a esto '{'{src}'}'</p>
+<img src={src} alt="logo de svelte"/>
+<img {src} alt="logo de svelte sin signo ="/>

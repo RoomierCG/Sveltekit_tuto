ddb87ce0f5d26efb782e57fa6f106dd6363af05f
RoomierCG <alex.granda2@hotmail.com>
2024-03-25 14:46:49 +0100

    Part-1/introduction: Firts component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+	<ul>
+		<li><a href="/">Inicio</a></li>
+		<li><a href="/basic-svelte">Basic Svelte</a></li>
+	</ul>
+</nav>
---
+++ src/routes/basic-svelte/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let name = "world";
+</script>
+
+<h1>Basic svelte</h1>
+
+<p>
+	Svelte esta compuesto por bloques de HTML, CSS and JavaScript que estan encapsulados bajo la
+	extension `.svlete`
+</p>
+
+<p>Ejemplo de uso de script: "hello {name}"</p>
